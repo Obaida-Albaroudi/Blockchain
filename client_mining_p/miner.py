f59fdf51054db9404b866f1c348bc05630dfe821
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2020-01-15 15:17:52 -0800

    Completed the MVP for today

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +31,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -44,11 +50,17 @@ if __name__ == '__main__':
     f.close()
 
     # Run forever until interrupted
+    coins=0
     while True:
-        r = requests.get(url=node + "/last_block")
+        print("Before r request")
+        r = requests.get(url=node + "last_block")
         # Handle non-json response
+        print("After r request")
         try:
+            print("before data request")
             data = r.json()
+            print("Data", data)
+            print("after data request")
         except ValueError:
             print("Error:  Non-json response")
             print("Response returned:")
@@ -56,15 +68,22 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data)
+        print("New Proof", new_proof)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
+        print("post_data", post_data)
 
-        r = requests.post(url=node + "/mine", json=post_data)
+        r = requests.post(url=node + "mine", json=post_data)
+        print("r", r)
         data = r.json()
+        print("data after mining", data)
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
+        if data["Response"]=="New Block Forged":
+            coins+=1
+            print(f"We have {coins} Lambda Coins")
         # print the message from the server.
-        pass
+        print(data["Response"])
